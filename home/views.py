003e2c7b054af8411bdeabb23f5dcf2a44bd213a
Ali Julaee Rad <alijeyrad@gmail.com>
2023-03-11 13:33:12 +0330

    universities view
    show all unis

--- home/views.py
+++ home/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
+from .models import University
 
 # Create your views here.
 
 
 def universities(request):
-    return render(request, 'home/universities.html')
+    universities = University.objects.all()
+    context = {
+        'universities': universities,
+    }
+
+    return render(request, 'home/universities.html', context)
