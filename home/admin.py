64baec737eab79049743d1096114ed7048317cf4
Ali Julaee Rad <alijeyrad@gmail.com>
2023-03-11 12:44:53 +0330

    models created
    University, City and Country models created
    Registered in admin
    filled with dummy info

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import University, City, Country
 
 # Register your models here.
+
+@admin.register(University)
+class UserAdmin(admin.ModelAdmin):
+    date_hierarchy = 'date_established'
+    empty_value_display = '-empty-'
+    list_display = ('name', 'uni_type', 'city', 'total_students',)
+    search_fields = ['name', 'city', 'overview',]
+    list_filter = ('city', 'uni_type',)
+
+
+admin.site.register(Country)
+
+admin.site.register(City)
--- home/models.py
+++ home/models.py
@@ -1,3 +1,59 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
+
+class University(models.Model):
+    class Meta:
+        verbose_name_plural = _('universities')
+
+    PUBLIC = 'public university'
+    PRIVATE = 'private university'
+    ARTS = 'liberal arts college'
+    COMMUNITY = 'community college'
+
+    TYPE_CHOICES = [
+        (PUBLIC, PUBLIC),
+        (PRIVATE, PRIVATE),
+        (ARTS, ARTS),
+        (COMMUNITY, COMMUNITY),
+    ]
+    
+    city = models.ForeignKey('City', on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    acronym = models.CharField(max_length=15)
+    logo_pic = models.ImageField(upload_to="logos")
+    overview =  models.TextField()
+    date_established = models.DateField()
+    total_students = models.PositiveIntegerField()
+    international_students = models.PositiveIntegerField()
+    website = models.URLField()
+    acceptance_rate = models.FloatField()
+
+    uni_type = models.CharField(
+        max_length=100,
+        choices=TYPE_CHOICES,
+    )
+
+    def __str__(self) -> str:
+        return f'{self.name}'
+
+class City(models.Model):
+    class Meta:
+        verbose_name_plural = _('cities')
+
+    name = models.CharField('city name', max_length=100,)
+    country = models.ForeignKey('Country', on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return f'{self.name}'
+
+
+class Country(models.Model):
+    class Meta:
+        verbose_name_plural = _('countries')
+
+    name = models.CharField('country name', max_length=100)
+
+    def __str__(self) -> str:
+        return f'{self.name}'
